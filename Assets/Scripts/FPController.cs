cdf66be6d5e93a1788aeca66117cf47146732f0f
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-25 17:32:21 +0200

    Added door to prefabs, made a dummy wall in the test room.

---
+++ Assets/Scripts/FPController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPController : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
