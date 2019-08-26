7cbbd4e14fd08e427acfcf903b3a074066c8da4b
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-26 20:04:42 +0200

    First version of movement created

--- Assets/Scripts/FPController.cs
+++ Assets/Scripts/FPController.cs
@@ -5,22 +5,43 @@ using UnityEngine;
 public class FPController : MonoBehaviour
 {
     [SerializeField] private Camera playerCamera;
+
+    [SerializeField] private float speed=5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        ChangeCursorMode();
     }
 
     private void Movement()
     {
         float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        Debug.Log(x+","+y);
+        float z = Input.GetAxis("Vertical");
+        x *= Time.deltaTime;
+        z *= Time.deltaTime;
+        transform.Translate(x*speed,0,z*speed);
+        Debug.Log(x+","+z);
+    }
+
+    private static void ChangeCursorMode()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else if (Cursor.lockState == CursorLockMode.None)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
     }
 }
