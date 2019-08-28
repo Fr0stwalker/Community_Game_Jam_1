4d86fa838acc08c1db1df750b8a3fbf3e76acd4a
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-28 13:25:44 +0200

    Work started on the interaction system for the game

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private float range=10f;
+    private void Update()
+    {
+        ProcessRaycast();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
+        {
+            if (hit.transform.CompareTag("Interactive"))
+            {
+                Debug.Log(hit.transform.name + " was hovered over");
+            }
+        }
+    }
+}
