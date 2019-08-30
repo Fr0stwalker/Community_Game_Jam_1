daaf4ec04fae61e86245d6dcd265f27fc7dd6ea1
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-30 19:22:53 +0200

    Added Item types for easier trigger response

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    enum ItemType
+    {
+        Key,
+        Spoon,
+        Fork,
+        Knife
+    }
+
+    [SerializeField] private ItemType itemType;
+}
